e40ab4002af96f5ac24a6848a51562f0060c6774
Mohammed Malik <malikmohammed@2067>
2025-08-13 21:31:44 +0530

    Used Api from openWeatherMap to show the real time Data

--- mini-project/src/SearchBox.jsx
+++ mini-project/src/SearchBox.jsx
@@ -2,7 +2,26 @@ import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import { useState } from 'react';
 const SearchBox = () => {
-    let [city,setCity]=useState("")
+    let [city,setCity]=useState("");
+    const API_URL="https://api.openweathermap.org/data/2.5/weather";
+    const API_KEY="9bb1fb4186d57d8a9296bebfdaa0edae";
+    let getWeatherInfo =async()=>{
+        let response=await fetch(`${API_URL}?q=${city}&appid=${API_KEY}`)
+        let jsonResponse=await response.json();
+        // console.log(jsonResponse);
+         let result={
+        temp : jsonResponse.main.temp,
+        tempMin : jsonResponse.main.temp_min,
+        tempMax : jsonResponse.main_temp,
+        humidity : jsonResponse.main.humidity,
+        feelsLike : jsonResponse.main.feels_like,
+        weather : jsonResponse.weather[0].description
+
+    }
+    console.log(result)
+   
+
+    }
     let handleChange=(evt)=>{
         setCity(evt.target.value);
 
@@ -10,11 +29,12 @@ const SearchBox = () => {
     let handleSubmit=(evt)=>{
         evt.preventDefault();
         console.log(city);
-        setCity("")
+        setCity("");
+        getWeatherInfo();
     }
   return (
     <div>
-      <form action="" onClick={handleSubmit}>
+      <form action="" onSubmit={handleSubmit}>
          <TextField id="city" label="cityName" variant="standard" required  value={city} onChange={handleChange}/>
         <Button variant="contained" type="submit" >Search</Button>
       </form>
